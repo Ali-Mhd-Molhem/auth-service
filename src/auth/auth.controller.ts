5c99ab6a14b9b35170df88761ff29c4a43ff4440
Ali Molhem <a.molhem@hardwework.com>
2025-09-24 17:55:23 +0400

    add rabbitmq connection

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Post, Get, Body, Query } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto, LoginDto, ValidateTokenDto } from './dto';
 import { ApiTags } from '@nestjs/swagger';
+import { MessagePattern } from '@nestjs/microservices';
+
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
@@ -17,9 +19,14 @@ export class AuthController {
     return this.authService.login(dto);
   }
 
-  @Post('validate')
-  async validateToken(@Body() dto: ValidateTokenDto) {
-    return this.authService.validateToken(dto.token);
+  // @Post('validate')
+  // async validateToken(@Body() dto: ValidateTokenDto) {
+  //   return this.authService.validateToken(dto.token);
+  // }
+
+  @MessagePattern({ cmd: 'validate_token' })
+  async validateToken(token: string) {
+    return this.authService.validateToken(token); // return user payload or null/throw
   }
 
   @Get('validate')
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,15 @@ async function bootstrap() {
   app.enableCors({
     origin: true,
     credentials: true,
+  });
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://guest:guest@localhost:5672'],
+      queue: 'auth_queue',
+      queueOptions: { durable: false },
+    },
   });
 
   const config = new DocumentBuilder()
@@ -34,6 +44,8 @@ async function bootstrap() {
 
   // Global prefix
   app.setGlobalPrefix('api');
+
+  await app.startAllMicroservices();
 
   const port = process.env.PORT || 3001;
   await app.listen(port);
